e1abeb7c639b10be7642bbe9ec79cbe987f7f5f6
Joshin Saju <126143102+Joshinx17@users.noreply.github.com>
2025-09-02 00:57:37 +0530

    Add files via upload

---
+++ 04_Methods/WithStrings.java
@@ -0,0 +1,12 @@
+public class WithStrings {
+    
+    static String greeting() {
+        String greet = "Hello World!";
+        return greet;
+    }
+    public static void main(String[] args) {
+        String message = greeting();
+        System.out.println(message);  
+    }
+    
+}
