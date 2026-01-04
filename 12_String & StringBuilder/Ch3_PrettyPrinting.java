40865ec3338995cbdabcc685ec4ab5eaa4f64b38
Joshin Saju <126143102+Joshinx17@users.noreply.github.com>
2026-01-04 20:57:28 +0530

    Delete 12_String & StringBuilder directory

--- 12_String & StringBuilder/Ch3_PrettyPrinting.java
+++
@@ -1,8 +0,0 @@
-public class Ch3_PrettyPrinting {
-    public static void main(String[] args) {
-        float a = 435.165437f; // Now print this number to only 2 decimal places.
-        System.out.printf("New formatted nummber is: %.2f \n", a);
-        System.out.println(Math.PI);
-        System.out.printf("Value of pi: %.5f", Math.PI);
-    }
-}
--- 12_String & StringBuilder/Ch6_StringBuilder.java
+++
@@ -1,9 +0,0 @@
-public class Ch6_StringBuilder { //  BTW note, StringBuilder is already a class, so we cant name this StringBuilder.java
-    public static void main(String[] args) {
-        StringBuilder builder = new StringBuilder(); // These are mutable just like arrays. 
-        for (int i = 0; i < 26; i++) {
-            char ch = (char) ('a' + i);
-            builder.append(ch); 
-        }   
-    }
-}
